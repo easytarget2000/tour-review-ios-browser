1e7e88cf57263c3e50fc4e53b4a6d84c4b381439
Mitch <michel@easy-target.eu>
2019-03-22 15:03:26 +0100

    Initialised empty Xcode project.

---
+++ TourReviewBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TourReviewBrowser
+//
+//  Created by Mitch on 22.03.19.
+//  Copyright © 2019 Easy Target. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
